9294cd2677f933e2fd9f8e80407b2ead6e6869e2
Doug Cox <dcox@kinsolresearch.com>
2015-03-23 15:23:02 -0700

    Initial work on an Http Client library that is extendable.

--- src/http_client.c
+++ src/http_client.c
@@ -5,6 +5,98 @@
  *      Author: Doug Cox
  */
 
+#include "http_client.h"
 
+unsigned char request_buffer[BUFF_SIZE];
 
+int get(const char *url) {
+	int transfer_len = 0;
 
+	make_request(HTTP_METHOD_GET, url, 0);
+
+	transfer_len = request(socket);
+	ASSERT_ON_ERROR(transfer_len);
+
+	return response(socket);
+}
+
+int post(const char *url, const char *data) {
+	int transfer_len = 0;
+
+	make_request(HTTP_METHOD_POST, url, data);
+
+	transfer_len = request(socket);
+	ASSERT_ON_ERROR(transfer_len);
+
+	return response(socket);
+}
+
+void make_request(const char *method, const char *url, const char *data) {
+	int transfer_len = 0;
+
+	clear_request_buffer();
+
+	strcpy((char *)request_buffer, method);
+	strcat((char *)request_buffer, url);
+	strcat((char *)request_buffer, " HTTP/1.1\r\n");
+#ifdef SERVER_NAME
+	strcat((char *)request_buffer, "Host: ");
+	strcat((char *)request_buffer, SERVER_NAME);
+	strcat((char *)request_buffer, "\r\n");
+#endif
+#ifdef AUTHORIZATION_HEADER
+	strcat((char *)request_buffer, "Authorization: ");
+	strcat((char *)request_buffer, AUTHORIZATION_HEADER);
+	strcat((char *)request_buffer, "\r\n");
+#endif
+#ifdef USERAGENT_HEADER
+	strcat((char *)request_buffer, "User-Agent: ");
+	strcat((char *)request_buffer, USERAGENT_HEADER);
+	strcat((char *)request_buffer, "\r\n");
+#endif
+#ifdef CONTENTTYPE_HEADER
+	strcat((char *)request_buffer, "Content-Type: ");
+	strcat((char *)request_buffer, CONTENTTYPE_HEADER);
+	strcat((char *)request_buffer, "\r\n");
+#endif
+	if (data) {
+		// TODO: sprintf and temp variable for Content-Length header
+		// strcat((char *)request_buffer, "Content-Length: ");
+		// strcat((char *)request_buffer, "\r\n");
+		strcat((char *)request_buffer, "\r\n");
+		strcat((char *)request_buffer, data);
+	} else {
+		strcat((char *)request_buffer, "Connection: close\r\n");
+	}
+	strcat((char *)request_buffer, "\r\n");
+}
+
+int request(socket) {
+	int transfer_len = 0;
+
+	transfer_len = sl_Send(socket, request_buffer, strlen((const char *)request_buffer), 0);
+	ASSERT_ON_ERROR(transfer_len);
+
+	return HTTP_SUCCESS;
+}
+
+int response(socket) {
+	int transfer_len = 0;
+
+	clear_request_buffer();
+
+	transfer_len = sl_Recv(socket, &request_buffer[0], MAX_BUFF_SIZE, 0);
+	ASSERT_ON_ERROR(transfer_len);
+
+	return HTTP_SUCCESS;
+}
+
+void print_request_buffer() {
+#ifdef UART_PRINT
+	UART_PRINT(request_buffer);
+#endif
+}
+
+void clear_request_buffer() {
+	memset(request_buffer, 0, sizeof(request_buffer));
+}
--- src/http_client.h
+++ src/http_client.h
@@ -8,6 +8,37 @@
 #ifndef HTTP_CLIENT_H_
 #define HTTP_CLIENT_H_
 
+#define MAX_BUFF_SIZE      1460
+#define BUFF_SIZE          MAX_BUFF_SIZE + 1
 
+#define HTTP_SUCCESS       0
+#define HTTP_FAILURE       -1
+
+#define HTTP_METHOD_GET      "GET"
+#define HTTP_METHOD_POST     "POST"
+
+#ifndef ASSERT_ON_ERROR
+#define ASSERT_ON_ERROR(error_code)\
+            {\
+                 if(error_code < 0) \
+                   {\
+                        ERR_PRINT(error_code);\
+                        return error_code;\
+                 }\
+            }
+#endif
+
+#include <string.h>
+
+int get(const char *url);
+int post(const char *url, const char *data);
+
+int request(int socket);
+int response(int socket);
+
+void make_request(const char *method, const char *url, const char *data);
+
+void clear_request_buffer();
+void print_request_buffer();
 
 #endif /* HTTP_CLIENT_H_ */
