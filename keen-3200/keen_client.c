65dfc7e240c1d95be41564479daab332f1c29028
Doug Cox <dcox@kinsolresearch.com>
2015-03-25 16:13:36 -0700

    Started working on keen_client

--- keen-3200/keen_client.c
+++ keen-3200/keen_client.c
@@ -5,4 +5,14 @@
  *      Author: Doug Cox
  */
 
+#include "keen_client.h"
 
+int add_event() {
+
+	return HTTP_SUCCESS;
+}
+
+int add_events() {
+
+	return HTTP_SUCCESS;
+}
--- keen-3200/keen_client.h
+++ keen-3200/keen_client.h
@@ -8,6 +8,13 @@
 #ifndef KEEN_CLIENT_H_
 #define KEEN_CLIENT_H_
 
+#define	SERVER_NAME        "api.keen.io"
+#define	CONTENTTYPE_HEADER "application/json"
+#define	USERAGENT_HEADER   "CC3200/0.1a"
 
+#include "http_client.h"
+
+int add_event();
+int add_events();
 
 #endif /* KEEN_CLIENT_H_ */
